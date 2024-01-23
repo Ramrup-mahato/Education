96ce8e525bf25412411593f7f92a3eb083974bc1
Ramrupmahato <ram@codermaniaa.com>
2024-01-23 14:39:39 +0545

    page done completed

--- components/Astrologer.js
+++ components/Astrologer.js
@@ -1,8 +1,16 @@
-import { View, Text, Image, TouchableOpacity, TextInput } from "react-native";
-import React, { useState } from "react";
+import {
+  View,
+  Text,
+  Image,
+  TouchableOpacity,
+  TextInput,
+  ScrollView,
+} from "react-native";
+import React, { useEffect, useState } from "react";
 import baba from "../assets/shavi.jpeg";
 import { AntDesign, Feather, Ionicons, FontAwesome6 } from "@expo/vector-icons";
 import { useNavigation } from "@react-navigation/native";
+import axios from "axios";
 
 const Message = ({ item }) => {
   return (
@@ -37,6 +45,8 @@ const Astrologer = () => {
   const [message, setMessage] = useState([
     { id: 1, name: "baba", message: "hello" },
   ]);
+  const [userData, setUserData] = useState([]);
+  // console.log(userData);
   const [text, setText] = useState("");
   const handleMessage = (value) => {
     setText(value);
@@ -51,22 +61,134 @@ const Astrologer = () => {
     setMessage([...message, newData]);
     setText("");
   };
+
+  const getApi = async () => {
+    await axios
+      .get("https://dummyjson.com/products")
+      .then((res) => {
+        setUserData(res.data.products);
+        // console.log(res.data.products);
+      })
+      .catch((err) => console.log(err));
+  };
+  useEffect(() => {
+    getApi();
+  }, [text]);
+
   return (
     <View style={{ width: "100%", position: "relative", height: "100%" }}>
-      {[1,2,].map((item, i) => (
-        <Image
-          key={i}
-          source={baba}
-          resizeMode="cover"
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <View
           style={{
             flex: 1,
-            width: null,
-            height: null,
+            justifyContent: "center",
+            alignItems: "center",
+            width: "100%",
+            padding: 10,
+            gap: 10,
           }}
-        />
-      ))}
+        >
+          {userData &&
+            userData.map((item, i) => (
+              // <Image
+              //   key={i}
+              //   source={baba}
+              //   resizeMode="cover"
+              //   style={{
+              //     flex: 1,
+              //     width: null,
+              //     height: null,
+              //   }}
+              // />
+              <View
+                style={{
+                  flex: 1,
+                  backgroundColor: "#412770",
+                  // margin: 5,
+                  padding: 20,
+                  elevation: 5,
+                  borderRadius: 10,
+                }}
+                key={i}
+              >
+                <View>
+                  <Image
+                    source={{ uri: item.images[0] }}
+                    width={300}
+                    height={300}
+                    style={{
+                      borderRadius: 10,
+                    }}
+                  />
 
-      <View
+                  <Text
+                    style={{
+                      color: "#fff",
+                      paddingTop: 10,
+                    }}
+                  >
+                    {item.brand}
+                  </Text>
+                  <Text
+                    style={{
+                      color: "#fff",
+                      fontSize: 10,
+                    }}
+                  >
+                    {item.category}
+                  </Text>
+                  <View
+                    style={{
+                      // width: "100%",
+                      flexDirection: "row",
+                      justifyContent: "space-between",
+                      alignItems: "center",
+                      padding: 10,
+                    }}
+                  >
+                    <Text style={{ color: "#fff", fontSize: 13 }}>
+                      Rs. {item.price}
+                    </Text>
+
+                    <View
+                      style={{
+                        flexDirection: "row",
+                        gap: 10,
+                        justifyContent: "center",
+                        alignItems: "center",
+                      }}
+                    >
+                      <AntDesign name="star" size={15} color="gold" />
+                      <Text style={{ color: "#fff", fontSize: 13 }}>
+                        {item.rating}
+                      </Text>
+                    </View>
+                  </View>
+                  <TouchableOpacity
+                    style={{
+                      width: "100%",
+                      backgroundColor: "#0971e8",
+                      justifyContent: "center",
+                      alignItems: "center",
+                      padding: 10,
+                      borderRadius: 50,
+                      elevation: 10,
+                    }}
+                    onPress={() => navigation.navigate("Dashboard")}
+                  >
+                    <Text
+                      style={{ color: "#fff", fontSize: 17, fontWeight: "800" }}
+                    >
+                      Add to Cart
+                    </Text>
+                  </TouchableOpacity>
+                </View>
+              </View>
+            ))}
+        </View>
+      </ScrollView>
+
+      {/* <View
         style={{
           position: "absolute",
           top: 0,
@@ -202,7 +324,7 @@ const Astrologer = () => {
             </View>
           </View>
         </View>
-      </View>
+      </View> */}
     </View>
   );
 };
